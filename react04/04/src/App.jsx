7bbbff4a21dddada087d67350046de0762ba5386
Aanchallll18 <kumariaachal002@gmail.com>
2025-04-28 13:28:53 +0530

    commit

--- react04/04/src/App.jsx
+++ react04/04/src/App.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import React, { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(10);
+  const [num, setNum] = useState(false);
+  const [ch, setCh] = useState(true);
+  const [Password, setPassword] = useState("");
 
   return (
     <>
-      <h1>hi</h1>
+      <h1 className='font-bold text-white text-3xl text-center mt-4'>Password Generator</h1>
+
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-6 text-black bg-gray-700 h-[180px]'>
+        <div className='flex shadow-md rounded-lg overflow-hidden mb-4 bg-amber-50 mt-5'>
+          <input 
+            type="text" 
+            value={Password}
+            className='outline-none w-full py-1 px-3'
+            placeholder='password'
+          />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
