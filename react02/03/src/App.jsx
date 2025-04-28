314161d88a1f8ed88a8247e7f0dc64a839d3aa93
Aanchallll18 <kumariaachal002@gmail.com>
2025-04-28 11:02:20 +0530

    commit

--- react02/03/src/App.jsx
+++ react02/03/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
  
   return (
     <>
-      <h1 className='text-blue-400 text-7xl font-bold justify-center text-center '>hello</h1>
+      <h1 className='text-blue-400 text-7xl font-bold justify-center text-center bg-amber-300 rounded-b-sm p-4 m-1 w-100'>hello</h1>
     </>
   )
 }
