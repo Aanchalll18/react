c230f52bfeca37d6dd0913b7d6a54596b969f23a
Aanchallll18 <kumariaachal002@gmail.com>
2025-04-28 12:36:01 +0530

    commit

--- react03/03/src/App.jsx
+++ react03/03/src/App.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+//import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("pink");
 
   return (
     <>
-      
+      {/* Use min-h-screen to ensure full screen height */}
+      <div className='w-full min-h-screen duration-200' style={{ backgroundColor: color }}>
+        <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+          <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl '>
+            test
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
