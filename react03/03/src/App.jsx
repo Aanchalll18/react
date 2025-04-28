89d7d22fbc72b60c6ecdc51f55855b93d208c53c
Aanchallll18 <kumariaachal002@gmail.com>
2025-04-28 12:53:46 +0530

    commit

--- react03/03/src/App.jsx
+++ react03/03/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 function App() {
   const [color, setColor] = useState("pink");
@@ -7,17 +7,29 @@ function App() {
     <>
       <div className='w-full min-h-screen duration-200' style={{ backgroundColor: color }}>
         <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
-          <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl font-bold border-3 border-red-600 text-red-600 mr-4'>
+          <div
+            className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl font-bold border-3 border-red-600 text-red-600 mr-4 cursor-pointer'
+            onClick={() => setColor('red')}
+          >
             red
           </div>
-          <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl border-3 font-bold border-green-600 text-green-600 mr-4'>
+          <div
+            className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl border-3 font-bold border-green-600 text-green-600 mr-4 cursor-pointer'
+            onClick={() => setColor('green')}
+          >
             green
           </div>
-          <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl border-3 border-amber-600 mr-4 font-bold text-amber-600'>
-            amber
+          <div
+            className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl border-3 border-purple-600 mr-4 font-bold text-purple-600 cursor-pointer'
+            onClick={() => setColor('purple')}
+          >
+            purple
           </div>
-          <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl border-3 font-bold text-blue-500'>
-            test
+          <div
+            className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl border-3 font-bold text-blue-500 cursor-pointer'
+            onClick={() => setColor('blue')}
+          >
+            blue
           </div>
         </div>
       </div>
