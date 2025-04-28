e8952b849dec1afc2b7992394def6c63bb95783e
Aanchallll18 <kumariaachal002@gmail.com>
2025-04-28 20:04:35 +0530

    commit

---
+++ react05/05/components/Input.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { useId } from "react";
+
+const Input = ({
+	label,
+	amount,
+	onAmountchange, 
+	onCurrencyChange,
+	currencyOptions = [],
+	selectCurrency = "inr",
+	amountDisable = false,
+	currencyDisable = false,
+	className = "",
+	placeholder = "Amount", // Add placeholder prop with a default value if not provided
+}) => {
+    const amountId=useId()
+
+	return (
+		<div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+			{/* Amount input */}
+			<div className="w-1/2">
+				<label 
+                htmlFor={amountId}
+                className="text-black/40 mb-2 inline-block">{label}</label>
+				<input
+                    id={amountId}
+					className="outline-none w-full bg-transparent py-1.5"
+					type="number"
+					placeholder={placeholder}
+					value={amount} 
+					onChange={(e) => onAmountchange && onAmountchange(e.target.value)} 
+					disabled={amountDisable} 
+				/>
+			</div>
+
+			{/* Currency selector */}
+			<div className="w-1/2 flex flex-wrap justify-end text-right">
+				<p className="text-black/40 mb-2 w-full">Currency Type</p>
+				<select
+					className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+					value={selectCurrency}
+					onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)} // Update the selected currency
+					disabled={currencyDisable} // Disable currency selector if necessary
+				>
+					{currencyOptions &&
+						currencyOptions.map((currency, index) => (
+							<option key={index} value={currency}>
+								{currency}
+							</option>
+						))}
+				</select>
+			</div>
+		</div>
+	);
+};
+
+export default Input;
--- react05/05/src/App.jsx
+++ react05/05/src/App.jsx
@@ -1,10 +1,19 @@
+import React from 'react'
+import { Input } from '../components'
 import { useState } from 'react'
+import useCurrencyInfo from './../hooks/useCurrencyInfo'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
+  const [from,setFrom]=useState("inr");
+  const [to,setTo]=useState("usd")
+  const [convertedAmount,setConvertedAmount]=useState(0);
+  const currencyInfo=useCurrencyInfo(from)
+  const options=Object.keys(currencyInfo)
 
   return (
     <>
+      <h1 className='font-bold text-blue-600'> Curreny Conveter</h1>
      
     </>
   )
