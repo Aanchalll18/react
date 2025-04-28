a427a133b0dcd48cfdd262e51cbd26aaae634a02
Aanchallll18 <kumariaachal002@gmail.com>
2025-04-28 10:58:25 +0530

    commit

---
+++ react02/03/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+function App() {
+ 
+  return (
+    <>
+      <h1 className='text-blue-400 text-7xl font-bold justify-center text-center '>hello</h1>
+    </>
+  )
+}
+
+export default App
