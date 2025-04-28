7f2ef41c8191c7fc8ecf2cf66bb65a67fc73004a
Aanchallll18 <kumariaachal002@gmail.com>
2025-04-29 00:32:12 +0530

    commit

--- react06/06/src/main.jsx
+++ react06/06/src/main.jsx
@@ -8,6 +8,7 @@ import Layouts from './Layouts/Layouts.jsx'
 import About from './components/About/About.jsx'
 import Home from './components/Home/Home.jsx'
 import Contact from './components/Contact/Contact.jsx'
+import User from './components/Users/User.jsx'
 
 const router =createBrowserRouter([
   {
@@ -25,6 +26,10 @@ const router =createBrowserRouter([
       {
         path:"contact",
         element:<Contact/>
+      },
+      {
+        path:"user/:UserId",
+        element:<User/>
       }
     ]
   }
